cccf023bd0ed735544c2c5cb6623508d701f3bef
durjoy <uniquedurjoy1@gmail.com>
2022-10-28 23:58:55 +0600

    first commit

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-light text-center p-3 text-dark mt-5'>
-            <h1 className='fw-bold fs-1'>&#169; Copyright by Programmer Bikash.</h1>
+            <h1 className='fw-bold fs-1'>&#169; Copyright by Durjoy</h1>
         </div>
     );
 };
